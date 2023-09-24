e4c903f390f549def614ee29209ff2fe3c9c1605
Pedro Lucas <pedrolucas122737@gmail.com>
2023-09-24 12:49:48 -0300

    Firt commit

---
+++ APICatalogo/Context/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using APICatalogo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace APICatalogo.Context
+{
+    public class AppDbContext : DbContext
+    {                          // confgurar contexto no ef             //Passando pra classe base (Dbcontext)
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        // Mapeamento das entidades
+        public DbSet<Categoria>? Categorias { get; set; }
+        public DbSet<Produto>? Produtos { get; set; } 
+    }
+}
---
+++ APICatalogo/Models/Categoria.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace APICatalogo.Models;
+
+// Já definiu o mapeamento no arquivo de contexto, o código abaixo é uma redundancia
+[Table("Categorias")]
+public class Categoria
+{
+
+    public Categoria()
+    {
+        Produtos = new Collection<Produto>(); // É uma boa pratica inicializar a coleção
+    }
+
+    [Key]// redundante
+    public int CategoriaId { get; set; }
+
+    [Required]
+    [StringLength(80)]
+    public string? Nome { get; set; }
+
+    [Required]
+    [StringLength(300)]
+    public string? ImagemUrl { get; set; }
+
+    // Cada categoria pode ter uma coleção de produtos
+    public ICollection<Produto>? Produtos { get; set; }
+
+}
+
