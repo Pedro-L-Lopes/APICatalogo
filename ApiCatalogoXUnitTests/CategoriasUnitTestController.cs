6f0e4686989b7f7b5ecfeed941f12282daf593e7
Pedro Lucas <pedrolucas122737@gmail.com>
2024-01-08 10:59:46 -0300

    Documentação e testes - Testes unitários 1

---
+++ ApiCatalogoXUnitTests/CategoriasUnitTestController.cs
@@ -0,0 +1,48 @@
+﻿using ApiCatalogo.Repository;
+using APICatalogo.Context;
+using APICatalogo.DTOs.Mappings;
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiCatalogoXUnitTests
+{
+    public class CategoriasUnitTestController
+    {
+        private IMapper mapper;
+        private IUnityOfWork repository;
+
+        public static DbContextOptions<AppDbContext> dbContextOptions { get; }
+
+        public static string conectionString = "server=localhost;port=3306;database=CatalogDB;user=root;password=";
+
+        static CategoriasUnitTestController()
+        {
+            dbContextOptions = new DbContextOptionsBuilder<AppDbContext>()
+                .UseMySql(conectionString)
+                .options;
+        }
+
+        public CategoriasUnitTestController()
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new MappingProfile());
+            });
+            mapper = config.CreateMapper();
+
+            var context = new AppDbContext(dbContextOptions);
+
+            //DbUnitTestsMackInitialize db = new DbUnitTestsMackInitialize();
+            //db.Seed(context);
+
+            repository = new UnityOfWork(context);
+        }
+
+        //Testes unitários
+    }
+}
---
+++ ApiCatalogoXUnitTests/DbUnitTestsMackInitialize.cs
@@ -0,0 +1,21 @@
+﻿using APICatalogo.Context;
+using APICatalogo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiCatalogoXUnitTests
+{
+    public class DbUnitTestsMackInitialize
+    {
+        public DbUnitTestsMackInitialize() { }
+
+        public void Seed(AppDbContext context)
+        {
+            context.Categorias.Add
+            (new Categoria { CategoriaId = 2, Nome = "Sucos", ImagemUrl = "sucos1.jpg" });
+        }
+    }
+}
